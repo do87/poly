c4148a14fc1abd2ecf7a62386f18a1fff98a329e
Dean Oren <deangili.oren@mail.schwarz>
2022-03-09 17:07:07 +0100

    add db package

---
+++ src/db/db.go
@@ -0,0 +1,43 @@
+package db
+
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+// DB is a wrapper around gorm
+type DB struct {
+	db *gorm.DB
+}
+
+// New returns a new database service
+func New(dialector gorm.Dialector) (*DB, error) {
+	db, err := gorm.Open(dialector)
+	if err != nil {
+		return nil, fmt.Errorf("error opening to DB: %s", err)
+	}
+
+	return &DB{
+		db: db,
+	}, nil
+}
+
+// GetDB returns a pointer to the DB
+func (d *DB) GetDB() *gorm.DB {
+	return d.db
+}
+
+// Close closes connection to the DB
+func (d *DB) Close() error {
+	sqlDB, err := d.db.DB()
+	if err != nil {
+		return err
+	}
+	return sqlDB.Close()
+}
+
+// Migrate runs table migrations
+func (d *DB) Migrate(tables ...interface{}) error {
+	return d.db.AutoMigrate(tables...)
+}
--- src/go.mod
+++ src/go.mod
@@ -6,11 +6,14 @@ require (
 	github.com/go-chi/chi/v5 v5.0.7
 	github.com/go-chi/render v1.0.1
 	go.uber.org/zap v1.21.0
+	gorm.io/gorm v1.23.2
 	moul.io/chizap v1.0.3
 )
 
 require (
 	github.com/go-chi/chi v1.5.4 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
 	go.uber.org/atomic v1.8.0 // indirect
 	go.uber.org/multierr v1.7.0 // indirect
 )
