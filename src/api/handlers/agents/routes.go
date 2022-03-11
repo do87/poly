5d8ea8f3cbc490aad52b377ac41283e723f954ad
Dean Oren <deangili.oren@mail.schwarz>
2022-03-11 13:54:03 +0100

    new routes

--- src/api/handlers/agents/routes.go
+++ src/api/handlers/agents/routes.go
@@ -3,9 +3,11 @@ package agents
 // setRoutes attaches product routes
 func (a *Agents) setRoutes() *Agents {
 	a.route.Get("/agents", nil)           // list all agents
-	a.route.Delete("/agent/{id}", nil)    // Delete an agent by ID
 	a.route.Post("/agent/{id}/poll", nil) // agent API polling
-	a.route.Get("/agents/keys", nil)      // list agent keys
-	a.route.Post("/agents/key", nil)      // create a new agent key
+	a.route.Delete("/agent/{id}", nil)    // Delete an agent by ID
+
+	a.route.Get("/agents/keys", nil)        // list agent keys
+	a.route.Post("/agents/key", nil)        // create a new agent key
+	a.route.Delete("/agents/key/{id}", nil) // Delete an agent key by ID
 	return a
 }
