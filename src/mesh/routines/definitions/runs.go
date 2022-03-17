db969dff233caacd5dfbe3110cf3dfeb22bc3f38
Dean Oren <deangili.oren@mail.schwarz>
2022-03-17 05:30:16 +0100

    add comments

--- src/mesh/routines/definitions/runs.go
+++ src/mesh/routines/definitions/runs.go
@@ -2,11 +2,15 @@ package definitions
 
 import "context"
 
+// AssignAgentToRuns handle agent assignment to created runs
 func AssignAgentToRuns(ctx context.Context) {
 }
 
+// FindInactiveAgents checks for agents that didn't make liveness calls > 10 minutes
+// and marks them as inactive
 func FindInactiveAgents(ctx context.Context) {
 }
 
+// If an agent is marked as inactive but has a running job it needs to be marked as cancelled
 func CancelRunsForInactiveAgents(ctx context.Context) {
 }
