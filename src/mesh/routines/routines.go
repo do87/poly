2ab5d2ab6d165fdda36fe12c277a7e6e84c03681
Dean Oren <deangili.oren@mail.schwarz>
2022-03-18 11:57:21 +0100

    move usecases to /mesh

--- src/mesh/routines/routines.go
+++ src/mesh/routines/routines.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/do87/poly/src/mesh/repos"
 	"github.com/do87/poly/src/mesh/routines/definitions"
+	"github.com/do87/poly/src/mesh/usecases"
 	"github.com/do87/poly/src/pkg/db"
 	"github.com/do87/poly/src/pkg/logger"
 	"github.com/go-co-op/gocron"
@@ -13,10 +14,12 @@ import (
 
 // Run runs all configured routines
 func Run(ctx context.Context, log logger.Log, db *db.DB) {
+	log.Info("♻️  starting mesh server routines")
 	cron := gocron.NewScheduler(time.UTC)
 
 	r := repos.New(db)
-	cron.Every(2).Second().SingletonMode().Do(definitions.AssignAgentToRuns, ctx, log, r)
+	agents := usecases.NewAgentsUsecase(r.Agents)
+	cron.Every(2).Second().SingletonMode().Do(definitions.AssignAgentToRuns, ctx, log, agents)
 	cron.Every(30).Second().Do(definitions.FindInactiveAgents, ctx, log, r)
 	cron.Every(1).Minute().Do(definitions.CancelRunsForInactiveAgents, ctx, log, r)
 	cron.StartBlocking()
